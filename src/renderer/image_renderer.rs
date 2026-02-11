478d285dd1c93b66a01e3cdf334be38ea6d2e39f
Chanyub.Park <mrchypark@gmail.com>
2026-02-12 01:48:06 +0900

    build: stabilize cross-platform native linkage and docs

--- src/renderer/image_renderer.rs
+++ src/renderer/image_renderer.rs
@@ -18,10 +18,12 @@ use crate::renderer::MapDebugOptions;
 ///
 /// ```no_run
 /// # fn foo() {
-/// use maplibre_native::{ImageRendererBuilder, Image};
+/// use std::num::NonZeroU32;
 ///
-/// let renderer = ImageRendererBuilder::new()
-///     .with_size(512, 512)
+/// use maplibre_native::{Image, ImageRendererBuilder};
+///
+/// let mut renderer = ImageRendererBuilder::new()
+///     .with_size(NonZeroU32::new(512).unwrap(), NonZeroU32::new(512).unwrap())
 ///     .build_static_renderer();
 ///
 /// renderer.load_style_from_url(&"https://demotiles.maplibre.org/style.json".parse().unwrap());
